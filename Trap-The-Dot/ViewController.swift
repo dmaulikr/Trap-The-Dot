1d6621123c7a7df45e4499a99d24c16de1bd1d96
reeonce <reeonce@yahoo.com>
2015-08-21 16:17:41 +0800

    Initial Commit

---
+++ Trap-The-Dot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trap-The-Dot
+//
+//  Created by Reeonce Zeng on 8/21/15.
+//  Copyright © 2015 reeonce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
